27114093139232ca87b09312206f86a3458571d9
ordephenrique99 <henriquepedro9999@gmail.com>
2025-04-12 00:33:36 +0000

    CRIAÇÃO DO FOR PARA MOVIMENTOS DA RAINHA - AJUSTES

--- xadrez.c
+++ xadrez.c
@@ -3,18 +3,25 @@
 int main() {
  
     int torre = 5, bispo = 5, rainha = 8;
-    int movimentos;
+    int movimentos = 1;
  
-    while(movimentos<torre){ 
-        movimentos++;
+    while(movimentos<=torre){ 
         printf("Movimento Torre %d - DIREITA\n", movimentos);
+        movimentos++;
     }
 
     printf("\n");
+    movimentos = 1;
     do{
-        movimentos++;
         printf("Movimento Bispo %d - CIMA/DIREITA\n", movimentos);
-    }while(movimentos<bispo);
+        movimentos++;
+    }while(movimentos<=bispo);
+
+    printf("\n");
+    movimentos = 1;
+    for(movimentos=1;movimentos<=rainha;movimentos++){
+        printf("Movimento Rainha %d - ESQUERDA\n", movimentos);
+    }
 
  
  
