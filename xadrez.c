c1b40d482715d611acea9d4fae071936a6d07c2e
ordephenrique99 <henriquepedro9999@gmail.com>
2025-04-12 00:29:09 +0000

    inclusão do DO WHILE para movimentos do bispo

--- xadrez.c
+++ xadrez.c
@@ -2,13 +2,19 @@
 
 int main() {
  
-    int torre = 5, bispo, rainha;
+    int torre = 5, bispo = 5, rainha = 8;
     int movimentos;
  
     while(movimentos<torre){ 
         movimentos++;
         printf("Movimento Torre %d - DIREITA\n", movimentos);
     }
+
+    printf("\n");
+    do{
+        movimentos++;
+        printf("Movimento Bispo %d - CIMA/DIREITA\n", movimentos);
+    }while(movimentos<bispo);
 
  
  
